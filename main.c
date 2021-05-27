d1bbbc70b20c08a9bb9a7b3748835f0c21b8d53a
Akbar Umar Alfaroq <akbarumar88@gmail.com>
2021-05-27 20:46:36 +0700

    TEST

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 #include <stdbool.h>
 #Halo Frans, Ezra, Dimas
 #Tes lagi gan
+#Tes lagi gan
+#Tes lagi gan
+#Tes lagi gan
+#Tes lagi gan
+#Tes lagi gan
+#Tes lagi gan
+#Tes lagi gan
 typedef struct barang{
     int id;
     char nama[50];
